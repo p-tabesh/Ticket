9bac5bdd98aa41e48780b2a8181b0783b0e66ee2
p-tabesh <pooyatabeshomom@gmail.com>
2024-12-19 10:11:05 +0330

    some change

--- Ticket.Test/Program.cs
+++ Ticket.Test/Program.cs
@@ -8,33 +8,3 @@ class MainTest
     }
 }
 
-class Test
-{
-    public int Salam { get; set; }
-    
-}
-
-class MyClass:Test
-{
-    public int Adad { get; set; }
-    public string Reshte { get; set; }
-}
-
-
-public class IntList
-{
-    private int[] _intArray = new int[10];
-    public void Add(int Adad)
-    {
-        _intArray.Append(Adad);
-    }
-}
-
-public class StringList
-{
-    private string[] _stringArray = new string[10];
-    public void Add(string Reshte)
-    {
-        _stringArray.Append(Reshte);
-    }
-}
