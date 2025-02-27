68f645bed23da8b73cdba879a6bf8c3fa815e057
p-tabesh <pooyatabeshomom@gmail.com>
2025-02-27 15:31:29 +0330

    implement some test

--- Ticket.Test/Program.cs
+++ Ticket.Test/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Ticket.Infrastructure.Context;
+using Ticket.Test;
+
+
+var options = new DbContextOptionsBuilder<TicketDbContext>().UseInMemoryDatabase("TicketTestingDb").Options;
+var dbContext = new TicketDbContext(options); 
+
+var test = new UnitTest1(dbContext);
+test.Test();
