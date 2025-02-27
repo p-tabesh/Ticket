910d21f4258ac34464650de446eda2c6504c3fae
p-tabesh <pooyatabeshomom@gmail.com>
2025-02-27 11:26:36 +0330

    change logic of account service

--- Ticket.Application/Services/AccountService.cs
+++ Ticket.Application/Services/AccountService.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Ticket.Application.Extentions;
+using Ticket.Domain.Entity;
 using Ticket.Infrastructure.Context;
 using Ticket.Infrastructure.UnitOfWork;
 
@@ -15,26 +16,28 @@ public class AccountService
     private readonly IConfiguration _configuration;
     private readonly TicketDbContext _dbContext;
     private readonly IDistributedCache _redisDistributedCache;
-    private static readonly string blackListKey = "tokenBlackList";
+
     public AccountService(IConfiguration configuration, TicketDbContext dbContext, IDistributedCache redisDistributedCache)
     {
         _configuration = configuration;
         _dbContext = dbContext;
         _redisDistributedCache = redisDistributedCache;
     }
-    public string GenerateToken(string username, string userRole)
+
+    public string GenerateToken(int userId, string userRole)
     {
         var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
         securityKey.KeyId = Guid.NewGuid().ToString();
+
         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
 
         var claims = new[]
         {
-            new Claim(JwtRegisteredClaimNames.Sub,username),
             new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
-            new Claim(ClaimTypes.Role,userRole)
+            new Claim(ClaimTypes.Role,userRole),
+            new Claim(JwtRegisteredClaimNames.NameId, userId.ToString())
         };
-        
+
         var token = new JwtSecurityToken(issuer: _configuration["Jwt:Issuer"],
             audience: _configuration["Jwt:Audience"],
             claims: claims,
@@ -45,25 +48,23 @@ public class AccountService
         return tokenString;
     }
 
-    public bool CheckUserIdentity(string username, string password)
+    public User GetUser(string username, string password)
     {
         using var UoW = new UnitOfWork(_dbContext);
         var user = UoW.UserRepository.GetByUsername(username.ToLower());
 
-        if (user == null)
-            throw new Exception("user doesnt exists");
+        if (user == null || !user.IsCorrectPassword(password.ToSha256()))
+            throw new Exception("invalid username or password");
 
-        if (user.IsCorrectPassword(password.ToSha256()))
-            return true;
-
-        return false;
+        return user;        
     }
+
     public void Logout(JwtSecurityToken token)
     {
-        var username = token.Claims.FirstOrDefault(t => t.Type == "sub")?.Value;
+        var userId = token.Claims.FirstOrDefault(t => t.Type == "nameid")?.Value;
         var jti = token.Claims.FirstOrDefault(t => t.Type == "jti")?.Value;
         _redisDistributedCache.SetString(jti,
-            username,
+            userId,
             new DistributedCacheEntryOptions() { AbsoluteExpiration = DateTime.Now.AddMinutes(10) });
     }
 
--- Ticket.Presentation/Controllers/AccountController.cs
+++ Ticket.Presentation/Controllers/AccountController.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using Ticket.Application.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ticket.Application.Models;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.StackExchangeRedis;
 
 namespace Ticket.Presentation.Controllers;
 
@@ -17,55 +12,22 @@ namespace Ticket.Presentation.Controllers;
 public class AccountController : Controller
 {
 
-    private readonly IConfiguration _configuration;
     private readonly AccountService _accountService;
-    private readonly IDistributedCache _redisCache;
     public AccountController(IConfiguration configuration, AccountService accountService, IDistributedCache redisDistributedCache)
     {
-        _configuration = configuration;
         _accountService = accountService;
-        _redisCache = redisDistributedCache;
     }
 
-    [AllowAnonymous]
+
     [HttpPost]
     [Route("login")]
-    public async Task<IActionResult> LoginWithCookie(string username, string password)
+    public IActionResult Login([FromBody] LoginModel loginModel)
     {
-        
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier, username),
-        };
-
-        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-        var principal = new ClaimsPrincipal(identity);
-        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-        await Console.Out.WriteLineAsync(HttpContext.User.Claims.ToList()[0].Value);
-        return Ok();
+        var user = _accountService.GetUser(loginModel.Username, loginModel.Password);
+        var tokenString = _accountService.GenerateToken(user.Id, loginModel.Rule);
+        return Ok(tokenString);
     }
 
-    [HttpPost]
-    [Route("user-login")]
-    public IActionResult LoginWithJwtToken([FromBody] LoginModel loginModel)
-    {
-        if (_accountService.CheckUserIdentity(loginModel.Username, loginModel.Password))
-        {
-            var tokenString = _accountService.GenerateToken(loginModel.Username,loginModel.Rule);
-            return Ok(tokenString);
-        }
-        return BadRequest("password incorrect");       
-    }
-
-    [HttpGet]
-    [Route("check-user")] 
-    public IActionResult CheckUser([FromHeader] string Authorization)
-    {
-        var tokenHeader = HttpContext.Request.Headers["Authorization"];
-        var tokenAuth = HttpContext.GetTokenAsync(JwtBearerDefaults.AuthenticationScheme, "Authorization");
-        var test = HttpContext.User.Claims;
-        return Ok();
-    }
 
     [Authorize]
     [Authorize(Policy = "Admin")]
@@ -86,3 +48,6 @@ public class AccountController : Controller
         }
     }
 }
+
+
+
